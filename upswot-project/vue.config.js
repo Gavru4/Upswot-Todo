555dfef6931f8318d19f8c08f2bec41cbb76aaab
Roman Havryliuk <gavru4r@gmail.com>
2022-08-07 23:07:51 +0300

    add dist

--- upswot-project/vue.config.js
+++ upswot-project/vue.config.js
@@ -1,10 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 module.exports = defineConfig({
   transpileDependencies: true,
-
-  configureWebpack: {
-    plugins: [new MiniCssExtractPlugin()],
-  },
 });
