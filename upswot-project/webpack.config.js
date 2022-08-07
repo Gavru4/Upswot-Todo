5ec95a2774039a64c2d9c8eb717164a7c2afad07
Roman Havryliuk <gavru4r@gmail.com>
2022-08-07 18:24:39 +0300

    update webpack

--- upswot-project/webpack.config.js
+++ upswot-project/webpack.config.js
@@ -1,26 +1,14 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  context: __dirname,
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: "babel-loader",
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"],
-      },
-    ],
-  },
-  plugins: [new HtmlWebPackPlugin(), new MiniCssExtractPlugin()],
+const config = {
+  // ...
+  plugins: [
+    // Whatever other plugins you may have
+  ],
 };
+
+const configWithTimeMeasures = new SpeedMeasurePlugin().wrap(config);
+configWithTimeMeasures.plugins.push(new MiniCssExtractPlugin({}));
+
+module.exports = configWithTimeMeasures;
