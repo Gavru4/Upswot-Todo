b8cb0230659b1d414a64fd27dcdf84d0bea0dce5
Roman Havryliuk <gavru4r@gmail.com>
2022-08-08 19:37:46 +0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 // const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = defineConfig({
-  publicPath: "/",
+  // publicPath: "/",
   transpileDependencies: true,
   // plugins: [new MiniCssExtractPlugin()],
 });
