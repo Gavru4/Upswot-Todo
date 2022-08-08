69288dec085cd37076e30cf66a3a3e1f1f28dd84
Roman Havryliuk <gavru4r@gmail.com>
2022-08-08 10:09:29 +0300

    update vue config

--- upswot-project/vue.config.js
+++ upswot-project/vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  plugins: [new MiniCssExtractPlugin()],
 });
