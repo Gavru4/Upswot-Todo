0ea5e19e3d50269fda11dc4399b021d258d02f08
Roman Havryliuk <gavru4r@gmail.com>
2022-08-08 11:14:36 +0300

    add MiniCssExtractPlugin

--- upswot-project/vue.config.js
+++ upswot-project/vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = defineConfig({
   publicPath: "/upswot-project",
-  // transpileDependencies: true,
-  // plugins: [new MiniCssExtractPlugin()],
+  transpileDependencies: true,
+  plugins: [new MiniCssExtractPlugin()],
 });
